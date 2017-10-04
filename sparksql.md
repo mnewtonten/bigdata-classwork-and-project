f8a1217c1b074f97117a8fbcc40e5368959216c8
2014-08-27 <mnewton@xena12.cs.trinity.edu>
2017-10-04 00:53:51 -0500

    Problemset 4 commit 3

--- sparksql.md
+++ sparksql.md
@@ -4,8 +4,8 @@
 
 2.  Highest employment rate was LAUCN48427 in February of 1990, where the unemployment rate is 54.1
 
-3.
+3.  Highest employment rate was LAUCT046347 in 58.9 in July of 2013, where the unemploment rate is 58.9
 
-4.
+4. Maine
 
 5.
---
+++ src/main/scala/sparksql/SSQL4.scala
@@ -0,0 +1,97 @@
+import scalafx.application.JFXApp
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+
+object SSQL4 extends App{
+  val spark = SparkSession.builder.master("spark://pandora00:7077").getOrCreate
+  import spark.implicits._
+  spark.sparkContext.setLogLevel("WARN")
+
+  val schema = StructType(Array(
+    StructField("series_id", StringType),
+    StructField("year", IntegerType),
+    StructField("period", StringType),
+    StructField("value", DoubleType),
+    StructField("footnote_codes", StringType)))
+
+        
+
+  //val data = spark.read.schema(schema).option("header", true).option("delimiter", "\t").csv("/data/BigData/bls/la/la.data.concatenatedStateFiles")
+
+  //Question 2&3
+  //val uRate = data.filter(substring('series_id,19,2) === "03").filter('period =!= "M13")
+  //val selURate = uRate.select(substring('series_id,0,18).as("ID"),'year,'period,'value )
+  //val lForce = data.filter(substring('series_id,19,2) === "06").filter('period =!= "M13").filter('value >= 10000)
+  //val selLForce = lForce.select(substring('series_id,0,18).as("ID"),'year,'period,'value.as("crap"))
+  //val combinedSHIT = selURate.join(selLForce, Seq("ID","year","period")).sort('value.desc).cache()
+
+
+  val secondSchema = StructType(Array(
+     StructField("id",StringType),
+     StructField("art", StringType),
+    StructField("area_code",StringType),
+    StructField("measure_code",IntegerType),
+    StructField("seasonal", StringType),
+    StructField("srd_code", IntegerType)))
+
+  val seriesData = spark.read.schema(secondSchema).option("header",true).option("delimiter", "\t").csv("/data/BigData/bls/la/la.series").cache()
+  
+  //data.filter('precip > 2).select('year * 12 + 'month).show
+  //println(data.filter('tmax > 100).count)
+  //data.groupBy('year).mean("tmax").orderBy("avg(tmax)").show
+
+  // question 2
+  //data.filter(substring('series_id,19,2) === "04").select(max('value)).show()
+
+  // question 3
+  //println(data.filter(substring('series_id,0,1) === "G").count)
+
+  System.out.println("~~L~O~O~K~~~O~U~T~~~B~E~L~O~W~~~B~I~G~~~D~A~T~A~~~I~N~C~O~M~I~N~G~~")
+
+  // question 4.a
+  //data.filter(substring('series_id,19,2) === "03").filter('period =!= "M13").groupBy('period).mean("value").describe().show()
+
+  // question 4.b
+  //data.filter(substring('series_id,19,2) === "03").filter('period === "M13").describe().show()
+ 
+  // question 1.c
+  //val monthlyUnemploymentRates = data.filter(substring('series_id,19,2) === "03").filter('period =!= "M13")
+  //monthlyUnemploymentRates.describe()
+  //System.out.println(monthlyUnemploymentRates)
+  
+  //combinedSHIT.show
+
+
+  val groupedSD = seriesData.groupBy('srd_code).count.sort('count.desc).show
+
+  System.out.println("~~W~H~E~W~~~T~H~A~T~~~W~A~S~~~S~O~M~E~~~B~I~G~~~D~A~T~A~~")
+
+
+  // question 1.d
+
+  //User defined function example
+  //myFunc = udf(("stateToNum", (s:String) => s.filter(_ != '\'').toInt)
+  //data.select(myFunc('state)).show()
+
+/*
+  user Defined Aggregation example
+  typed & untyped
+  object WeightedAverage extends Aggregator[TempData2,(Double,Double),Double] {
+    def bufferEncoder
+    daf finish(reduction: (Double,Double)):Double = r._1/r._2
+    def merge(
+    def reduce(b: (Double,Double), a:TempData2):(Double,Double) = {
+      val (vsum,wsum = b
+      (vsum + a.tmax*a.precip,wsum+a.precip)
+    }
+    def zero : (Double,Doube) = (0.0, 0.0)
+  }
+*/
+
+  //data.createOrReplaceTempView("tdata")
+  //spark.sql("select tmax from tdata").show
+
+  spark.stop()
+
+}
